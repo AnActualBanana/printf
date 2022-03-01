b23139c3d6dbbed449691d3fe15d5200abc47466
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-03-01 12:16:18 -0800

    attempt to sync our printf source code

---
+++ _printf.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: a character string
+ * Return: the number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va list args;
+	unsigned int count;
+
+	va_start(args, format);
+
+	for (count = 0; count != '\0'; count ++)
+	{
+
+	}
+}
