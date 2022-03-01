1219d1949ec106662c37ab7bcdbf08e136bcc21c
AnActualBanana <loganmcclain59@gmail.com>
2022-03-01 09:28:37 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -3,6 +3,14 @@
  *
  */int _printf(const char *format, ...)
  {
+	 va_list args;
+	 unsigned int count;
+	 va_start(args, format);
+	 for (count = 0; count != "/0"; count++)
+	 {
+		 switch(
+	 }
 
 
+	 
  }
