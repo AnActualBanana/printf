eacf16324e89b36c8b1d30e1210f950d64025209
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-03-01 11:10:47 -0800

    main.h file for main branch_jel

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* includes */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/* prototypes */
+
+int _putchar(char c);
+
+int _printf(const char *format, ...);
+
+#endif /* MAIN_H */
