56f334340891360edc16816537381f97eb6bc538
AnActualBanana <loganmcclain59@gmail.com>
2022-03-02 13:04:42 -0800

    added cases for 's' and 'i', still working on 'i'

--- test/_printf.c
+++ test/_printf.c
@@ -5,26 +5,51 @@
  {
 	 va_list args;
 	 unsigned int count;
-	 char c;
+	 unsigned int printed;
+	 char print_index;
+	 char *string;
+	 unsigned int decimal;
+	 unsigned int digit;
+	 unsigned int string_count;
 	 va_start(args, format);
 	 for (count = 0; format[count] != '\0'; count++)
 	 {
 		 if (format[count] != '%')
 		 {
-		 c = format[count];
-		 _putchar(c);
+		 print_index = format[count];
+		 _putchar(print_index);
 		 }
 		 else if (format[count] == '%')
 		 {
 			 count++;
-			 c = format[count];
-			 switch (c)
+			 print_index = format[count];
+			 switch (print_index)
 			 {
+			 case 's' :
+				 string = va_arg(args, char *);
+				 for (string_count = 0; string[string_count] != '\0'; string_count++)
+				 {
+					 print_index = string[string_count];
+					 _putchar(print_index);
+					 printed++;
+				 }
+				 break;
 			 case '%' :
-				 _putchar(c);
+				 _putchar(print_index);
+				 printed++;
+				 break;
+			 case 'i' :
+				 decimal = va_arg(args, int);
+				 while (decimal > 0)
+				 {
+					 digit = decimal % 10;
+					 _putchar(digit + '0');
+					 printed++;
+					 decimal = decimal / 10;
+				 }
 				 break;
 			 }
 		 }
 }
-	 return (0);
+	 return (printed);
  }
