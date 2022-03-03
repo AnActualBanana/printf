62647abf312a3c29007d2390da7ec74d68412f08
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-03-03 08:24:34 -0800

    update to main printf function file

--- _printf.c
+++ _printf.c
@@ -9,31 +9,51 @@
 
 int _printf(const char *format, ...)
 {
-	 va_list args;
+	va_list args;
 
-	 unsigned int count;
-	 char c;
+	unsigned int count;
+	unsigned int printed;
+	char print_index;
+	char *string;
+	unsigned int decimal;
+	unsigned int digit;
+	unsigned int string_count;
 
-	 va_start(args, format);
+	va start(args, format);
 
-	 for (count = 0; format[count] != '\0'; count++)
-	 {
-		 if (format[count] != '%')
-		 {
-			 c = format[count];
-			 _putchar(c);
-		 }
-		 else if (format[count] == '%')
-		 {
-			 count++;
-			 c = format[count];
-			 switch (c)
-			 {
-			 case '%':
-				 _putchar(c);
-				 break;
-			 }
-		 }
-	 }
- }
+	for (count = 0; format[count != '\0'; count++)
+		{
+			if (format[count] != '%')
+			{
+				print_index = format[count];
+				_putchar(print_index);
+			}
+			else if (format[count] == '%')
+			{
+				count++;
+				print_index = format[count];
+				switch (print_index)
+				{
+				case 'c':
+					print_index = va_arg(args, int);
+					_putchar(print_index);
+					printed++;
+					break;
+				case 's':
+					string = va_arg(args, char *);
+					for (string_count = 0; string[string_count] != '\0'; string_count++)
+					{
+						print_index = string[string_count];
+						_putchar(print_index);
+						printed++;
+					}
+					break;
+				case '%':
+					_putchar(print_index);
+					printed++;
+					break;
+				}
+			}
+		}
+		     return (printed);
 }
