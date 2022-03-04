2aa31a14dd7877389c88c85df3bdf0a0eed5bd8a
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-03-03 20:47:02 -0800

    bettiquette updates to conversion specifier source code

--- _conversion_s.c
+++ _conversion_s.c
@@ -1,16 +1,22 @@
 #include "main.h"
-/*
- *
- */int _conversion_s(char *string)
- {
-	 unsigned int string_count;
-	 unsigned int printed = 0;
-	 char print_index;
-	 for (string_count = 0; string[string_count] != '\0'; string_count++)
-	 {
-		 print_index = string[string_count];
-		 _putchar(print_index);
-		 printed++;
-	 }
-	 return(printed);
- }
+
+/**
+ * _conversion_s - function formats conversion character s for string
+ * @string: char pointer to string in argument list
+ * Return: integer count of printed characters
+ */
+
+int _conversion_s(char *string)
+{
+	unsigned int string_count;
+	unsigned int printed = 0;
+	char print_index;
+
+	for (string_count = 0; string[string_count] != '\0'; string_count++)
+	{
+		print_index = string[string_count];
+		_putchar(print_index);
+		printed++;
+	}
+	return (printed);
+}
